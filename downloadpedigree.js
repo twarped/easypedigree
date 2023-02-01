36eee3660153799dc18bc54ab423d3c5836c8d3b
twarped <48340527+twarped@users.noreply.github.com>
2023-01-31 19:51:50 -0700

    Update downloadpedigree.js

--- downloadpedigree.js
+++ downloadpedigree.js
@@ -22,5 +22,27 @@
         {})
         , "ROOTNAMES": (() => {
             window.ROOTNAMES = []
-        })(), "DESCROOT": (() => { window.DESCROOT = [document.querySelector("#main-content-section > tree-app").shadowRoot.querySelector("#pages > tree-pedigree").shadowRoot.querySelector("#pedigree").shadowRoot.querySelector("#pedigree > div:nth-child(1) > pedigree-couple-renderer.node[data-test-couple-ahnentafel='1']"), ...Array.from(document.querySelector("#main-content-section > tree-app").shadowRoot.querySelector("#pages > tree-pedigree").shadowRoot.querySelector("#pedigree").shadowRoot.querySelectorAll("#pedigree > div:nth-child(1) > pedigree-couple-renderer.desc-node")).filter(c => { try { return c.shadowRoot.querySelector("div.couple-wrapper").parentNode.querySelector("button.append-pedigree").title.includes("Collapse") } catch (e) { return false } })]; return window.DESCROOT; })().map(couple => { return couple.coupleId.slice(0, couple.coupleId.includes(".") ? -4 : -2) }).reduce((p, c, i) => ({ ...p, [window.DESCROOT[i].attributes["data-test-couple-ahnentafel"].nodeValue]: c }), {}) })); var a = document.createElement("a"); a.href = dataStr; a.download = prompt("Pedigree (file)name:", "pedigree") + ".json"; document.body.appendChild(a); a.click(); setTimeout(() => document.body.removeChild(a), 0);
+        })(), "DESCROOT": (() => { window.DESCROOT = [document.querySelector("#main-content-section > tree-app")
+            .shadowRoot.querySelector("#pages > tree-pedigree")
+            .shadowRoot.querySelector("#pedigree")
+            .shadowRoot.querySelector("#pedigree > div:nth-child(1) > pedigree-couple-renderer.node[data-test-couple-ahnentafel='1']")
+            , ...Array.from(document.querySelector("#main-content-section > tree-app")
+            .shadowRoot.querySelector("#pages > tree-pedigree")
+            .shadowRoot.querySelector("#pedigree")
+            .shadowRoot.querySelectorAll("#pedigree > div:nth-child(1) > pedigree-couple-renderer.desc-node"))
+            .filter(c => { try { 
+                return c.shadowRoot.querySelector("div.couple-wrapper")
+                .parentNode.querySelector("button.append-pedigree").title.includes("Collapse") 
+            } catch (e) { return false } })]; 
+        return window.DESCROOT; })().map(couple => { 
+            return couple.coupleId.slice(0, couple.coupleId.includes(".") ? -4 : -2) }).reduce((p, c, i) => ({
+                ...p, [window.DESCROOT[i].attributes["data-test-couple-ahnentafel"].nodeValue]: c 
+            }), {}) })); 
+    var a = document.createElement("a"); 
+    a.href = dataStr; 
+    a.download = prompt("Pedigree (file)name:", "pedigree") + ".json"; 
+    if (a.download == null) return false;
+    document.body.appendChild(a); 
+    a.click(); 
+    setTimeout(() => document.body.removeChild(a), 0);
 })();
