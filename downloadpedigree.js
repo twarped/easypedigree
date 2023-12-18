c6fe4430003d08250b9556ab1d1a8393721051ff
twarped <samthebeastofall@gmail.com>
2023-12-17 20:21:44 -0700

    Updated downloadpedigree.js to 3.0

--- downloadpedigree.js
+++ downloadpedigree.js
@@ -1,63 +1,82 @@
-//version 2.1 code. better data arrays. I forgot to upload 2.0, but basically FS reprogrammed their pedigree interface, so I had to reprogram mine.
+// version 3.0 code. more data for better formatting and readability. not compatible with formatpedigree.js 2.0 or lower.
 
 (() => {
     try {
-        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify({
-            "ROOT_PEOPLE_DATA": [
-                [document.body.querySelector(".listCss_l14v1yn6").querySelector("[data-testid='asc-couple-0.0']")].map(e => [e.innerText.replaceAll("\n", " ").split("  ").map((e, i) => {
-                    if (i % 2 == 1) return e.split(" ")[2];
-                    return e;
-                }), e.dataset.testid].flat()), ...Array.from(document.body.querySelector(".listCss_l14v1yn6").querySelectorAll("button.iconButtonCss_i1memprp.lowCss_l18l38sv")).filter(e => e.ariaLabel.includes("Collapse")).map(e => [...e.parentNode.parentNode.innerText.replaceAll("\n", " ").split("  ").map((e, i) => {
-                    if (i % 2 == 1) return e.split(" ")[2];
-                    return e;
-                }), e.parentNode.parentNode.dataset.testid]).map((e, i, a) => [
-                    [document.body.querySelector(".listCss_l14v1yn6").querySelector(`[data-testid='asc-couple-${e[e.length-1].split("-").splice(-1)[0].split(".").map((e, i) => { if (i) return Math.floor(e/2); return e - 1; }).join(".")}']`)].map(e => [e.innerText.replaceAll("\n", " ").split("  ").map((e, i) => {
-                        if (i % 2 == 1) return e.split(" ")[2];
-                        return e;
-                    }), e.dataset.testid]).flat().flat(), e
-                ])
+        
+        let baseURI = document.body.querySelector('[data-testid="pedigree"]').baseURI;
+        let pedigreeId = baseURI.split('pedigree/')[1].split('/')[1];
+        let ancestorName = document.body.querySelector(`[data-testid="open-person-sheet:${pedigreeId}"]`).parentNode.querySelector('[class^="nameWrapCss"]').innerText.replace('\n', ' ');
+
+        let pedigreeData = {
+            'BASE_URI': baseURI,
+            'PEDIGREE_ID': pedigreeId,
+            'ANCESTOR_NAME': ancestorName,
+            'ROOT_PEOPLE_DATA': [
+                [...document.body.querySelectorAll('[data-testid^="asc-couple-"]')].map(e => [
+                    e.dataset.testid,
+                    [...e.querySelector('[class^="coupleCss"]').querySelectorAll('[class^="addWrapperCss"],[class^="nameWrapCss"]')].map(e => e.className.includes('addWrapperCss') ? [
+                        e.innerText, null, null
+                    ] : [
+                        e.innerText, e.parentNode.querySelector('[class^="lifespan"]').innerText.split('\n')
+                    ].flat())
+                ].flat())
             ].flat().map(e => {
                 return {
-                    "husband": {
-                        "name": e[0],
-                        "id": (e[0] == "ADD SPOUSE" ? 0 : e[1])
+                    'husband': {
+                        'name': e[1][0],
+                        'lifespan': e[1][1],
+                        'id': e[1][2],
                     },
-                    "wife": {
-                        "name": (e[0] == "ADD SPOUSE" ? e[1] : e[2]),
-                        "id": (e[1] == "ADD SPOUSE" ? 0 : (e[2] == "ADD SPOUSE" ? 0 : (e[0] == "ADD SPOUSE" ? e[2] : e[3])))
+                    'wife': {
+                        'name': e[2][0],
+                        'lifespan': e[2][1],
+                        'id': e[2][2],
                     },
-                    "coupleId": (e[e.length - 1])
+                    'testid': e[0],
                 }
             }),
-            "DESC_PEOPLE_DATA": [...Array.from(document.body.querySelector(".listCss_l11vv4r5").querySelectorAll("button.iconButtonCss_i1memprp.lowCss_l18l38sv")).filter(e => e.ariaLabel.includes("Collapse")).map(e => [...e.parentNode.parentNode.innerText.replaceAll("\n", " ").split("  ").map((e, i) => {
-                if (i % 2 == 1) return e.split(" ")[2];
-                return e;
-            }), e.parentNode.parentNode.dataset.testid])].map(e => {
-                return {
-                    "husband": {
-                        "name": e[0],
-                        "id": (e[0] == "ADD SPOUSE" ? 0 : e[1])
+            'DESC_PEOPLE_DATA': [
+                [...document.body.querySelectorAll('[data-testid^="desc-couple-"]')].map(e => [
+                    e.dataset.testid,
+                    [...e.querySelector('[class^="coupleCss"]').querySelectorAll('[class^="addWrapperCss"],[class^="nameWrapCss"]')].map(e => e.className.includes('addWrapperCss') ? [
+                        e.innerText, null, null
+                    ] : [
+                        e.innerText, e.parentNode.querySelector('[class^="lifespan"]').innerText.split('\n')
+                    ].flat())
+                ].flat())
+            ].flat().map(e => {
+                return e[1][0] == 'ADD CHILD' ? {
+                    'unknown': 'ADD CHILD',
+                } : {
+                    'husband': {
+                        'name': e[1][0],
+                        'lifespan': e[1][1],
+                        'id': e[1][2],
                     },
-                    "wife": {
-                        "name": (e[0] == "ADD SPOUSE" ? e[1] : e[2]),
-                        "id": (e[1] == "ADD SPOUSE" ? 0 : (e[2] == "ADD SPOUSE" ? 0 : (e[0] == "ADD SPOUSE" ? e[2] : e[3])))
+                    'wife': {
+                        'name': e[2][0],
+                        'lifespan': e[2][1],
+                        'id': e[2][2],
                     },
-                    "coupleId": (e[e.length - 1])
+                    'testid': e[0],
                 }
             }),
-            "ROOT": Array.from(document.body.querySelector(".listCss_l14v1yn6").querySelectorAll("button.iconButtonCss_i1memprp.lowCss_l18l38sv")).filter(e => e.ariaLabel.includes("Collapse")).map(e => e.parentNode.parentNode.dataset.testid),
-            "DESCROOT": Array.from(document.body.querySelector(".listCss_l11vv4r5").querySelectorAll("button.iconButtonCss_i1memprp.lowCss_l18l38sv")).filter(e => e.ariaLabel.includes("Collapse")).map(e => e.parentNode.parentNode.dataset.testid),
-            "VERSION": 2.1
-        }));
-        var a = document.createElement("a");
+            'ROOT_EXPAND': [...document.body.querySelector('[aria-label^="Ancestors of"]').querySelectorAll('button[data-testid^="pedigree-expand-up"]')].filter(e => e.ariaLabel.includes('Collapse')).map(e => e.getAttribute('aria-describedby')),
+            'DESCROOT_EXPAND': [...document.body.querySelector('[aria-label^="Descendants of"]').querySelectorAll('button[data-testid^="pedigree-expand-down"]')].filter(e => e.ariaLabel.includes('Collapse')).map(e => e.getAttribute('aria-describedby')),
+            'VERSION': 3.0
+        };
+        console.log(pedigreeData);
+
+        let dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(pedigreeData));
+        let a = document.createElement('a');
         a.href = dataStr;
-        a.download = prompt("Pedigree (file)name:", "pedigree") + ".fsped";
-        if (a.download == "null.fsped") return false;
+        a.download = prompt(`Pedigree (file)name defaults to:\n${ancestorName}.fsped`, ancestorName) + '.fsped';
+        if (a.download == 'null.fsped') return false;
         document.body.appendChild(a);
         a.click();
         setTimeout(() => document.body.removeChild(a), 0);
     } catch (error) {
-        alert("You have to be in FamilySearch portrait mode for this to work.");
+        alert('You have to be in FamilySearch landscape or portrait mode for this to work.');
         console.error(error);
         return;
     }
